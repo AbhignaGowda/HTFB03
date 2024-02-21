adec3aafa99f941164dfb8b6cad866e911275b75
AbhignaGowda <abhignagowda07@gmail.com>
2024-02-21 08:04:37 +0530

    commit after adding comments for well maintained code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Upload from './components/Upload'
 import Home from './components/Home';
@@ -7,16 +6,18 @@ import Login from './components/Login';
 import About from './components/About';
 import Contact from './components/Contact';
 
+//App.jsx file where all files and components are rendered and routing is done using react-router-dom
+
 const App=()=> {
 
   const handleLogin = (userData) => {
-    // Your login logic here
     console.log('Login data:', userData);
   };
   return(
     
     <div className="min-h-screen">
   <div className="gradient-bg-welcome">
+  {/* Routing using react-router-dom */}
   <Routes>
   
         <Route path="/home" element={<Home />} />
